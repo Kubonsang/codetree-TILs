1f494352c5eb0b626cbf48bd613e1eb7fc97956c
Kubonsang <165915671+Kubonsang@users.noreply.github.com>
2024-08-31 21:01:41 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240831/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void aaa(int &a, int &b){
+    if(a > b){
+        a *= 2;
+        b += 10;
+    }
+    else{
+        b *= 2;
+        a += 10;
+    }
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    aaa(a, b);
+    cout << a << " " << b;
+    return 0;
+}
