1d997f3229018fbd8b20887041bcbc67010bea8f
Kubonsang <165915671+Kubonsang@users.noreply.github.com>
2024-09-01 19:46:47 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240901/2개씩 그룹짓기/group-of-pairs.cpp
+++ 240901/2개씩 그룹짓기/group-of-pairs.cpp
@@ -4,15 +4,21 @@
 using namespace std;
 
 int main() {
-    int size, arr[1001];
+    int size, arr[2001];
     cin >> size;
-    for(int i = 0; i < size; i++){
+    for(int i = 0; i < size * 2; i++){
         cin >> arr[i];
     }
 
-    sort(arr, arr + size);
+    sort(arr, arr + size * 2);
 
-    cout << arr[0] + arr[size - 1];
+    int max = 0;
+    for(int i = 0; i < size * 2; i++){
+        if(arr[i] + arr[size * 2 - 1 - i] > max){
+            max = arr[i] + arr[size * 2 - 1 - i];
+        }
+    }
+    cout << max;
 
     return 0;
 }
