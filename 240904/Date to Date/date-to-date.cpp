508e81af0d783b2e013bbb0d69d18431b4c32f3a
Kubonsang <165915671+Kubonsang@users.noreply.github.com>
2024-09-04 18:20:01 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240904/Date to Date/date-to-date.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int months[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    int m1, m2, d1, d2;
+    int answer = 0;
+
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    for(int i = m1 + 1; i <= m2; i++){
+        answer += months[i];
+    }
+
+    answer += d2- d1;
+
+    cout << answer;
+
+    return 0;
+}
