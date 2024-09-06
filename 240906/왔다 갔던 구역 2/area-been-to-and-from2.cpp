1dd6440f6ec98e0f6d216cbb3f967c769ea5b2fb
Kubonsang <165915671+Kubonsang@users.noreply.github.com>
2024-09-06 22:27:15 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240906/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int currentPos = 1000, deltaPos, n;
+    char direct;
+    vector<int> arr(2001);
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> deltaPos >> direct;
+        if(direct == 'L'){
+            deltaPos *= -1;
+        }
+
+        if(direct == 'L'){
+            for(int j = currentPos; j > currentPos + deltaPos; j--) {
+                arr[j]++;
+            }
+        }
+        else{
+            for(int j = currentPos; j < currentPos + deltaPos; j++) {
+                arr[j]++;
+            }
+        }
+
+        currentPos += deltaPos;
+    }
+
+    int size = 0;
+    for(int i = 0; i < 2001; i++){
+        if(arr[i] >= 2){
+            size++;
+        }
+    }
+
+    cout << size;
+
+    return 0;
+}
