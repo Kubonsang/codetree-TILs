4b7da4c7232f2d49a6d937bf33e4ecd00ebe640d
Kubonsang <165915671+Kubonsang@users.noreply.github.com>
2024-09-10 22:08:31 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240910/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
+++ 240910/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
@@ -11,25 +11,18 @@ int main() {
         cin >> arr[i];
     }
 
-    int max = 1, cnt = 1;
-    for(int i = 1; i < n; i++){
-        if(arr[i] > arr[i - 1] && arr[i] > t && arr[i - 1] > t){
+    int max = 0, cnt = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] > t){
             cnt++;
         }
-        else {
-            cnt = 1;
+        else{
+            cnt = 0;
         }
         if(max < cnt){
             max = cnt;
         }
     }
-
-    if(max == 1){
-        cout << 0;
-        return 0;
-    }
     cout << max;
-
-
     return 0;
 }
