96f0ce530973e30c452a6ad763b5be50fe78789c
Kubonsang <165915671+Kubonsang@users.noreply.github.com>
2024-09-10 21:52:07 +0900

    Title: 연속되는 수 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240910/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int max = 1, cnt = 1;
+    for(int i = 1; i < n; i++){
+        if((arr[i] >= 0 && arr[i - 1] >= 0) || (arr[i] < 0 && arr[i - 1] < 0)){
+            cnt++;
+        }
+        else {
+            cnt = 1;
+        }
+        if(max < cnt){
+            max = cnt;
+        }
+    }
+
+    cout << max;
+
+
+    return 0;
+}
