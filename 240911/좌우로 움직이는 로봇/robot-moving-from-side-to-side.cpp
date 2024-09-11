de1fcb9754f50f468cb834b3cf68eae2c9031bb3
Kubonsang <165915671+Kubonsang@users.noreply.github.com>
2024-09-11 23:37:03 +0900

    Title: 좌우로 움직이는 로봇, Time: 20ms, Memory: 2MB, Status: Runtime Error - CodeTree

--- 240911/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 240911/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    int n, m, deltaSec, curPos = 0;
+    int n, m, deltaSec, curPos = 0, aSize = 0, bSize = 0;
     char direct;
     vector<int> a, b;
 
@@ -11,12 +11,12 @@ int main() {
     for(int i = 0; i < n; i++){
         cin >> deltaSec >> direct;
         if(direct == 'L'){
-            for(int j = 0; j < deltaSec; j++){
+            for(int j = 0; j <= deltaSec; j++){
                 a.push_back(curPos--);
             }
         }
         else{
-            for(int j = 0; j < deltaSec; j++){
+            for(int j = 0; j <= deltaSec; j++){
                 a.push_back(curPos++);
             }
         }
@@ -27,22 +27,29 @@ int main() {
     for(int i = 0; i < m; i++){
         cin >> deltaSec >> direct;
         if(direct == 'L'){
-            for(int j = 0; j < deltaSec; j++){
+            for(int j = 0; j <= deltaSec; j++){
                 b.push_back(curPos--);
             }
         }
         else{
-            for(int j = 0; j < deltaSec; j++){
+            for(int j = 0; j <= deltaSec; j++){
                 b.push_back(curPos++);
             }
         }
     }
 
+    for(int i = 0; i < 100000; i++){
+        a.push_back(a.back());
+        b.push_back(b.back());
+    }
 
+
+
+    
 
     int answer = 0;
-    for(int i = 1; i < a.size(); i++){
-        if(a[i] == b[i]){
+    for(int i = 2; i < a.size(); i++){
+        if(a[i] == b[i] && a[i - 1] != b[i - 1]){
             answer++;
         }
     }
