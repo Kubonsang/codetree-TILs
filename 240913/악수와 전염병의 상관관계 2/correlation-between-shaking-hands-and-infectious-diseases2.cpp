2b4ed223d8989d4f8e8a88af23d2adbe6edcf766
Kubonsang <165915671+Kubonsang@users.noreply.github.com>
2024-09-13 19:06:17 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240913/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
+++ 240913/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -12,7 +12,7 @@ int main() {
     IsInfected[0] = 0;
     for(int i = 1; i <= n; i++){
         IsInfected[i] = (i == p) ? 1 : 0;
-        handshakes[i] = k;
+        handshakes[i] = 0;
     }
     int time, x, y;
     for(int i = 0; i < t; i++){
@@ -23,15 +23,18 @@ int main() {
     for(int i = 1; i <= 250; i++){
         x = timeLine[i].first;
         y = timeLine[i].second;
-        if(IsInfected[x] == 1 && handshakes[x] > 0){
+        if(IsInfected[x] == 1 && handshakes[x] < k){
+            if(IsInfected[y] == 1){
+                handshakes[y]++;
+            }
             IsInfected[x] = 1;
             IsInfected[y] = 1;
-            handshakes[x]--;
+            handshakes[x]++;
         }
-        else if (IsInfected[y] == 1 && handshakes[y] > 0){
+        else if (IsInfected[y] == 1 && handshakes[y] < k){
             IsInfected[x] = 1;
             IsInfected[y] = 1;
-            handshakes[y]--;
+            handshakes[y]++;
         }
     }
 
