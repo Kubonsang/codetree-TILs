e8415c75aa8866b762ea593bd3dffc84997760e3
Kubonsang <165915671+Kubonsang@users.noreply.github.com>
2024-09-20 18:30:36 +0900

    Title: 격자 위의 편안한 상태, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240920/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
+++ 240920/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -6,7 +6,7 @@ vector<vector<int>> arr(100, vector<int>(100));
 int n, m, dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 
 bool isInvalid(int r, int c){
-    return (r == 0 || c == 0) && (r == n - 1 || c == n - 1);
+    return !((r >= 1 && r <= n - 2) && (c >= 1 && c <= n - 2));
 }
 
 bool isComfortable(int r, int c){
@@ -25,6 +25,9 @@ bool isComfortable(int r, int c){
 int main() {
     int r, c;
     cin >> n >> m;
+    r--;
+    c--;
+
     for(int i = 0; i < m; i++){
         cin >> r >> c;
         r--;
